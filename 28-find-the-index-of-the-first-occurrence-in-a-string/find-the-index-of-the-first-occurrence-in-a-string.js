1da27b63f8c9190cd611c2ad0a2e50d78ad6de2a
Rodrigo Soares <rodrigohths@gmail.com>
2024-03-20 11:22:11 -0300

    Time: 53 ms (54.58%) | Memory: 48.6 MB (81.80%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,15 +4,5 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-  for (let i = 0; i < haystack.length; i++) {
-    for (let j = 0; j < needle.length; j++) {
-      if (haystack[i + j] !== needle[j]) {
-        break
-      }
-      if (j === needle.length - 1) {
-        return i
-      }
-    }
-  }
-  return -1
+  return haystack.indexOf(needle) 
 }
