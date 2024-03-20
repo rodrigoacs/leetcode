d98d7a6f9b53f8b63747a30bc958512fd47b4f36
Rodrigo Soares <rodrigohths@gmail.com>
2024-03-20 09:27:46 -0300

    Time: 58 ms (54.42%) | Memory: 50.1 MB (61.28%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,18 +3,18 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-  s = s.split("")
   let stack = []
 
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
-      stack.push(s[i])
+    let char = s[i]
+    if (char === '(' || char === '{' || char === '[') {
+      stack.push(char)
     } else {
       if (stack.length === 0) return false
       let last = stack.pop()
-      if (s[i] === ')' && last !== '(') return false
-      if (s[i] === ']' && last !== '[') return false
-      if (s[i] === '}' && last !== '{') return false
+      if (char === ')' && last !== '(') return false
+      if (char === '}' && last !== '{') return false
+      if (char === ']' && last !== '[') return false
     }
   }
 
